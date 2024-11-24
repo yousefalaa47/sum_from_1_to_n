c503f0e6c8cc9adc22ad9a3161619c6c7d128a5d
yousefalaa47 <86966633+yousefalaa47@users.noreply.github.com>
2024-11-24 18:20:42 +0200

    Add files via upload

---
+++ src/sum_from_1_to_n/Sum_from_1_to_n.java
@@ -0,0 +1,13 @@
+
+package sum_from_1_to_n;
+import java .util.* ;
+public class Sum_from_1_to_n {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long n = input.nextLong();
+        System.out.println((n*(n+1))/2);
+    }
+}
+    
+    
+
